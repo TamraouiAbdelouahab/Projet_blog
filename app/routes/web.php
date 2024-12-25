b15afa02408ea972f6c60b4dea17aa24aa516ad8
AyoubyAmazu <ayoubamazu@gmail.com>
2024-12-25 15:57:31 +0100

    securiter done

--- app/routes/web.php
+++ app/routes/web.php
@@ -10,18 +10,18 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->name('dashboard');
-
-
-Route::resource('/dashboard/article',ArticleController::class);
-Route::resource('/dashboard/category',CategoryController::class);
-Route::resource('/dashboard/tag',TagController::class);
-
 
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/public', [App\Http\Controllers\HomeController::class, 'publicIndex'])->name('public.public.index');
 Route::get('/public/article/{id}', [App\Http\Controllers\HomeController::class, 'publicShow'])->name('public.public.show');
+
+Route::group(['middleware'=>['role:admin']] ,function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard'); 
+    Route::resource('/dashboard/article',ArticleController::class);
+    Route::resource('/dashboard/category',CategoryController::class);
+    Route::resource('/dashboard/tag',TagController::class);
+
+});
